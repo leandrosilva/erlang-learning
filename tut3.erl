f95df07c8f7c21df5d348dad47f6c9d5e9dc68ae
Leandro Silva <leandrodoze@gmail.com>
2009-02-10 07:58:12 -0200

    Structural chances

--- tut3.erl
+++
@@ -1,7 +0,0 @@
--module(tut3).
--export([convert_length/1]).
-
-convert_length({centimeter, X}) ->
-    {inch, X / 2.54};
-convert_length({inch, Y}) ->
-    {centimeter, Y * 2.54}.
--- tut4.erl
+++
@@ -1,8 +0,0 @@
--module(tut4).
-
--export([list_length/1]).
-
-list_length([]) ->
-    0;    
-list_length([First | Rest]) ->
-    1 + list_length(Rest).
